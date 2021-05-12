b04eafbb33d981fb9dd319e848dcdf33fa295cbd
Sven Waschkut <sven@waschkut.de>
2021-05-12 20:06:05 +0200

    update bash script to get correct git main and submodule pull

--- update-pan-os-php-invalid-ssl.bat
+++ update-pan-os-php-invalid-ssl.bat
@@ -1,9 +1,10 @@
 SET PATH=%PATH%;%CD%\php;%CD%\Console2;%CD%\git\bin
 
-cd pan-os-php
 
 git -c user.name=test -c user.email=test@test.com stash
 git clean -f
-git -c http.sslVerify=false pull origin main
+git reset --hard origin/main
+git -c http.sslVerify=false pull origin main --recurse-submodules
+
 
 pause
--- update-reset-github.bat
+++
@@ -1,7 +0,0 @@
-SET PATH=%PATH%;%CD%\php;%CD%\Console2;%CD%\git\bin
-
-cd pan-os-php
-
-git reset --hard origin/main
-
-pause
