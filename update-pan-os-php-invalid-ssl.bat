71ceddcff7c671db2d085d64bfa9c034cef18a15
Sven Waschkut <sven@waschkut.de>
2021-05-25 20:58:53 +0200

    update bash script 'update-pan-os-php.bat'

--- update-pan-os-php-invalid-ssl.bat
+++ update-pan-os-php-invalid-ssl.bat
@@ -2,9 +2,7 @@ SET PATH=%PATH%;%CD%\php;%CD%\Console2;%CD%\git\bin
 
 
 git -c user.name=test -c user.email=test@test.com stash
-git clean -f
-git reset --hard origin/main
-git -c http.sslVerify=false pull origin main --recurse-submodules
-
+git submodule init
+git -c http.sslVerify=false submodule update --remote
 
 pause
--- update-pan-os-php.bat
+++ update-pan-os-php.bat
@@ -2,9 +2,8 @@ SET PATH=%PATH%;%CD%\php;%CD%\Console2;%CD%\git\bin
 
 
 git -c user.name=test -c user.email=test@test.com stash
-git clean -f
-git reset --hard origin/main
-git pull origin main --recurse-submodules
+git submodule init
+git submodule update --remote
 
 
 pause
